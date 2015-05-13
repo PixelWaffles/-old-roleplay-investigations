eef7f57941ea48e58e4c50ad3763501e0c443bf1
ginsederp <ginsederp@derpymail.org>
2015-05-13 19:55:51 +0800

    Added parent constructor call in ListDiv.

--- public/scripts/rp/html/ListDiv.js
+++ public/scripts/rp/html/ListDiv.js
@@ -3,9 +3,11 @@ var rp = rp || {};
 rp.html = rp.html || {};
 
 // Class
-rp.html.ListDiv = new rp.html.DynamicDiv();
+rp.html.ListDiv = Object.create(rp.html.DynamicDiv.prototype);
 rp.html.ListDiv.prototype.parent = rp.html.DynamicDiv.prototype;
 
-rp.html.ListDiv.prototype.constructor = function() {
-  // TODO Write ListDiv class
+rp.html.ListDiv.prototype.constructor = function(_elementId, _para) {
+  this.parent.call(_elementId, _para);
+
+  return;
 };
