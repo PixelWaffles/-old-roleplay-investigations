4abf5ff398a7904578550584a5cf588927adf9c6
ginsederp <ginsederp@derpymail.org>
2015-05-17 20:59:23 +0800

    Fixed inheritance of ListDiv.

--- public/scripts/rp/html/ListDiv.js
+++ public/scripts/rp/html/ListDiv.js
@@ -3,14 +3,14 @@ var rp = rp || {};
 rp.html = rp.html || {};
 
 // Class
-rp.html.ListDiv = Object.create(rp.html.DynamicDiv.prototype);
-rp.html.ListDiv.prototype.parent = rp.html.DynamicDiv.prototype;
-
-rp.html.ListDiv.prototype.constructor = function(_elementId, _para) {
+rp.html.ListDiv = function(_elementId, _para) {
   this.parent.call(_elementId, _para);
 
   return;
 };
+
+rp.html.ListDiv.prototype = Object.create(rp.html.DynamicDiv.prototype);
+rp.html.ListDiv.prototype.parent = rp.html.DynamicDiv.prototype;
 
 rp.html.ListDiv.prototype.initHtml = function() {
   //TODO Write html initialization.
