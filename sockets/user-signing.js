efeb1021d56fec880a9a93aac110397d4e74a8b7
ginsederp <ginsederp@derpymail.org>
2015-05-18 16:40:29 +0800

    Added user sign out broadcast.

--- sockets/user-signing.js
+++ sockets/user-signing.js
@@ -4,7 +4,12 @@ var events = require('../events');
 events.on('user-sign-in-successful', broadcastSignInSuccessful);
 
 function broadcastSigning(_socket) {
-  // Empty
+  _socket.on('disconnect', function() {
+    if(_socket['$user']) {
+      broadcastSignOut(_socket['$user']);
+    }
+    return;
+  });
   return;
 };
 
@@ -16,6 +21,18 @@ function broadcastSignInSuccessful(_user) {
   });
 
   console.log("User " + _user + " signed in successful.");
+
+  return;
+};
+
+function broadcastSignOut(_user) {
+  io.sockets.emit('user-signing', {
+    'time': Date.now()
+  , 'user': _user
+  , 'type': 'SIGNOUT'
+  });
+
+  console.log("User " + _user + " signed out.");
 
   return;
 };
