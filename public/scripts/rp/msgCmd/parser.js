412cf34146e51b174b1c6b6426a3d791d61ede7d
ginsederp <ginsederp@derpymail.org>
2015-10-13 01:06:59 +0000

    Created client side chat command parser stub.

---
+++ public/scripts/rp/msgCmd/parser.js
@@ -0,0 +1,14 @@
+// This parser is responsible for converting user input in-text message commands into server read commands.
+
+// Namespace
+var rp = rp || {};
+rp.msgCmd = rp.msgCmd || {};
+
+// Singleton
+rp.msgCmd.parser = new function() {
+  "use strict";
+  this.parse = function(_para) {
+    // TODO Write parser.
+    return;
+  };
+};
