ef6ffa4a270b81c64126aca6c0c2823abe5e1f82
ginsederp <ginsederp@derpymail.org>
2015-10-17 09:55:27 +0000

    Added test command function stub.

---
+++ public/scripts/rp/msgCmd/cmdFunc/test.js
@@ -0,0 +1,12 @@
+// A test command function designed to echo display the command's parameters and message string index.
+
+// Namespace
+var rp = rp || {};
+rp.msgCmd = rp.msgCmd || {};
+rp.msgCmd.cmdFunc = rp.msgCmd.cmdFunc || {};
+
+rp.msgCmd.cmdFunc.test = function(_parentMessage, _commandSelf) {
+  // STUB
+  // TODO Move displayCommandReceived() functionality here.
+  return;
+};
