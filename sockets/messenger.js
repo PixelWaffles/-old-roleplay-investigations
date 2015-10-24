6b20750d355136d3bc19c1c838fe96d4601c7bbf
ginsederp <ginsederp@derpymail.org>
2015-10-24 09:19:17 +0000

    Moved literal in socket emit 'message-client' to createMessageToBroadcast().

--- sockets/messenger.js
+++ sockets/messenger.js
@@ -1,6 +1,7 @@
 var io = require('./io');
 
 var escape = require('escape-html');
+var _ = require('underscore')
 
 function handleMessage(_socket) {
   _socket.on('message-server', function(_data) {
@@ -14,14 +15,22 @@ function handleMessage(_socket) {
       
       return;
     }
+    
+    var messageToBroadcast = createMessageToBroadcast();
+    
+    
 
-    io.sockets.emit('message-client', {
-      'time': Date.now() // Time of Server on Message Sent.
-    , 'user': escape(_socket['$user'])
-    , 'message': escape(_data.message)
-    , 'commands': _data.commands
-    });
+    io.sockets.emit('message-client', messageToBroadcast);
     return;
+    
+    function createMessageToBroadcast() {
+      return {
+        'time': Date.now() // Time of Server on Message Sent.
+      , 'user': escape(_socket['$user'])
+      , 'message': escape(_data.message)
+      , 'commands': _data.commands
+      };
+    }
   });
 
   return;
