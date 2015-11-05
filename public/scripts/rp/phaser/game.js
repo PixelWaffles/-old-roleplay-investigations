b9baa7c3acaab49e15824e5efe28a0b9013ced52
ginsederp <ginsederp@derpymail.org>
2015-11-05 09:01:14 +0000

    Added Phaser key functions: preload(), create(), and update().

--- public/scripts/rp/phaser/game.js
+++ public/scripts/rp/phaser/game.js
@@ -1,8 +1,18 @@
-/* global Phaser */
+/*
+  globals
+    Phaser
+    preload
+    create
+    update
+ */
 
 // Namespace
 var rp = rp || {};
 rp.phaser = rp.phaser || {};
 
 // The game
-rp.phaser.game = new Phaser.Game(1920, 1080, Phaser.AUTO, 'stage-viewport');
+rp.phaser.game = new Phaser.Game(1920, 1080, Phaser.AUTO, 'stage-viewport', {
+  preload: preload
+, create: create
+, update: update
+});
---
+++ public/scripts/stage-viewport.js
@@ -0,0 +1,13 @@
+/* global rp */
+
+function preload() {
+  return;
+}
+
+function create() {
+  return;
+}
+
+function update() {
+  return;
+}
